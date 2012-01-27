f4779adf064940595ceac00bb46a7d149d6aa9bf
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-01-27 12:11:49 +0100

    Took the manaserv example data for now

---
+++ scripts/poker.lua
@@ -0,0 +1,16 @@
+-- Modul Poker
+
+module("poker", package.seeall)
+
+-- Klassen
+dofile("example/serverdata/scripts/poker/pokergame.lua")
+dofile("example/serverdata/scripts/poker/pokercard.lua")
+dofile("example/serverdata/scripts/poker/pokercardcolor.lua")
+dofile("example/serverdata/scripts/poker/pokercardvalue.lua")
+dofile("example/serverdata/scripts/poker/pokercardstack.lua")
+dofile("example/serverdata/scripts/poker/pokerplayer.lua")
+dofile("example/serverdata/scripts/poker/pokerconstants.lua")
+dofile("example/serverdata/scripts/poker/pokerpot.lua")
+dofile("example/serverdata/scripts/poker/pokerspade.lua")
+dofile("example/serverdata/scripts/poker/pokerspaderater.lua")
+dofile("example/serverdata/scripts/poker/pokerconstants.lua")
